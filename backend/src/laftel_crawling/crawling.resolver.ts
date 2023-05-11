c066a84991d49f9590959f769c27516c4fa4164a
min96 <ajdcnd131@naver.com>
2023-05-11 22:08:34 +0900

    🏷️  guard 설정

--- backend/src/laftel_crawling/crawling.resolver.ts
+++ backend/src/laftel_crawling/crawling.resolver.ts
@@ -1,13 +1,17 @@
 import { Resolver, Query, Args, Mutation } from '@nestjs/graphql';
 import { PrismaService } from 'prisma/prisma.service';
 import { CrawlingService } from './crawling.service';
+import { UseGuards } from '@nestjs/common';
+import { AdminGuard } from 'src/auth/auth.admin.guard';
+
 
 @Resolver()
 export class CrawlingResolver {
   constructor(private prisma: PrismaService, private crawlingService: CrawlingService) {}
 
   @Query(()=> String)
-  async test(): Promise<any> {
+  @UseGuards(AdminGuard)
+  async test(): Promise<string> {
     return this.crawlingService.fetchData();
   }
   
--- backend/src/review/review.resolver.ts
+++ backend/src/review/review.resolver.ts
@@ -4,12 +4,15 @@ import { Review } from './review.model';
 import {CreateInputReview} from './dto/create-review.dto';
 import { CurrentUser } from 'src/auth/current-user';
 import { User } from '@prisma/client';
+import { UseGuards } from '@nestjs/common';
+import { AuthGuard } from 'src/auth/auth.guard';
 
 @Resolver(() => Review)
 export class ReviewResolver {
   constructor(private reviewService: ReviewService) {}
 
     @Mutation(()=>Review)
+    @UseGuards(AuthGuard)
     async createReview(
       @CurrentUser() user:User,
       @Args('input') input : CreateInputReview ): Promise<Review>{
