52881b538d347ba394086f62fad99bb9da852c50
kimdong <parktest0325@gmail.com>
2023-05-30 01:37:53 +0900

    Init: 릴리즈 버전 인프라 환경 구축

---
+++ backend/entrypoint.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+# Wait for PostgreSQL to be ready
+until pg_isready --timeout=0 --dbname=$POSTGRES_DB --host=$POSTGRES_HOST --port=$POSTGRES_PORT --username=$POSTGRES_USER; do
+  sleep 1
+done
+
+# Run Prisma migrations
+npx prisma migrate deploy
+
+# Start the NestJS app
+npm start
