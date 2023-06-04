6f15cfe4821dedfaf1ccd047876f8085ad71b0c0
dongkim <parktest0325@gmail.com>
2023-06-04 21:02:18 +0900

    Fix: wait for elasticsearch

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
+# Wait for Elasticsearch to be ready
+until curl -sS --fail http://elasticsearch:9200 -o /dev/null; do
+  echo "Waiting for Elasticsearch to be ready..."
+  sleep 1
+done
+
 # Wait for PostgreSQL to be ready
 until pg_isready --timeout=0 --dbname=$POSTGRES_DB --host=$POSTGRES_HOST --port=$POSTGRES_PORT --username=$POSTGRES_USER; do
   sleep 1
